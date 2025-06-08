b0963c408c7ef3b635eef310e6924a0e26ea235d
ROCucu - MS <michael.strimbu@gmail.com>
2025-06-08 01:16:51 -0700

    Train Station, Done

--- main.ts
+++ main.ts
@@ -1,1 +1,235 @@
-
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        if (Jumps == 0) {
+            Jumps += 1
+        }
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy < 50) {
+        if (Jumps > 0) {
+            mySprite.vy += -100
+            Jumps += -1
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile12`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        if (info.score() >= 5) {
+            Ticket += 1
+            info.changeScoreBy(-5)
+        }
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    if (controller.B.isPressed()) {
+        tiles.setTileAt(location, assets.tile`myTile3`)
+        Axe += 1
+    }
+})
+let mySprite: Sprite = null
+let Jumps = 0
+Jumps = 1
+let Ticket = 0
+let Axe = 0
+info.changeScoreBy(5)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+scene.setBackgroundColor(9)
+tiles.setCurrentTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(mySprite, sprites.builtin.forestTiles0)
+controller.moveSprite(mySprite, 100, 0)
+game.onUpdateInterval(10, function () {
+    mySprite.vy += 3
+})
+forever(function () {
+    if (mySprite.isHittingTile(CollisionDirection.Right) && mySprite.tileKindAt(TileDirection.Right, assets.tile`myTile1`)) {
+        if (Axe == 1) {
+            animation.runImageAnimation(
+            mySprite,
+            [img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . c c . . . . . . . 
+                . . . . . c c c c c . . . . . . 
+                . . . . . c c c c c c . . . . . 
+                . . . . . c c c c c . . . . . . 
+                . . . . . . . c c . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . c c . . . . . . . . 
+                . . . . c c c c c . . . . . . . 
+                . . . . c c c c c c . . . . . . 
+                . . . . c c c c c . . . . . . . 
+                . . . . . . c c . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . c c b b . . . . . . . 
+                . . . . c c c c b . . . . . . . 
+                . . . . c c c c c . . . . . . . 
+                . . . . . c c c c . . . . . . . 
+                . . . . . . c c c . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . c c . . . . . . . . 
+                . . . . c c c c c . . . . . . . 
+                . . . . c c c c c c . . . . . . 
+                . . . . c c c c c . . . . . . . 
+                . . . . . . c c . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . c c . . . . . . . 
+                . . . . . c c c c c . . . . . . 
+                . . . . . c c c c c c . . . . . 
+                . . . . . c c c c c . . . . . . 
+                . . . . . . . c c . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . b b . . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . . . . b b . . . . . . 
+                . . . . . . . . . c c . . . . . 
+                . . . . . . . c c c c c . . . . 
+                . . . . . . . c c c c c c . . . 
+                . . . . . . . c c c c c . . . . 
+                . . . . . . . . . c c . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . b b b . . . . . . . . 
+                . . . . . . b b b . . . . . . . 
+                . . . . . . . b b b . . . . . . 
+                . . . . . . . . b b b . . . . . 
+                . . . . . . . . . b c c . . . . 
+                . . . . . . . . . . c c c c c . 
+                . . . . . . . . . . c c c c c c 
+                . . . . . . . . . . c c c c c . 
+                . . . . . . . . . . c c . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . b b b . . . . . . . . 
+                . . . . . . b b b . . . . . 1 . 
+                . . . . . . . b b b . . . . 1 . 
+                . . . . . . . . b b b . . . 1 . 
+                . . . . . . . . . b c c . . . 1 
+                . . . . . . . . . . c c c c c . 
+                . . . . . . . . . . c c c c c c 
+                . . . . . . . . . . c c c c c . 
+                . . . . . . . . . . c c . . . 1 
+                . . . . . . . . . . . . . . 1 1 
+                `,img`
+                . . . . . . f f f f . . . . . . 
+                . . . . f f f 2 2 f f f . . . . 
+                . . . f f f 2 2 2 2 f f f . . . 
+                . . f f f e e e e e e f f f . . 
+                . . f f e 2 2 2 2 2 2 e e f . . 
+                . . f e 2 f f f f f f 2 e f . . 
+                . . f f f f e e e e f f f f . . 
+                . f f e f b f 4 4 f b f e f f . 
+                . f e e 4 1 f d d f 1 4 e e f . 
+                . . f e e d d d d d d e e f . . 
+                . . . f e e 4 4 4 4 e e f . . . 
+                . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+                . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+                . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+                . . . . . f f f f f f . . . . . 
+                . . . . . f f . . f f . . . . . 
+                `],
+            100,
+            false
+            )
+            pause(900)
+            tiles.setTileAt(tiles.getTileLocation(16, 10), assets.tile`transparency16`)
+            tiles.setTileAt(tiles.getTileLocation(16, 9), assets.tile`transparency16`)
+            tiles.setWallAt(tiles.getTileLocation(16, 10), false)
+            tiles.setWallAt(tiles.getTileLocation(16, 9), false)
+            tiles.setTileAt(tiles.getTileLocation(16, 11), assets.tile`myTile4`)
+            Axe += -1
+        }
+    } else if (mySprite.isHittingTile(CollisionDirection.Right) && mySprite.tileKindAt(TileDirection.Right, assets.tile`myTile10`)) {
+        tiles.setWallAt(tiles.getTileLocation(64, 8), false)
+    }
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,99 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`4b00100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b0a0c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b0a0d0a0c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b0a0a0a0a0a0a0a0c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b0a0a0a0a0a0a0a0a0a0c000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b0a0a0a0c0000000b0a0a0a0a0a0a0a0a0a0a0a0c000000000a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b0a0a0a0a0a0c000000000909090909090909090000000000000a00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000090909090900000000000909090909090909090000000000000a000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000909090909000000000009090909090f09090900000000000011000000000000000000000000000000000000000000000000000000000000000000000000000000000700000000000000000910091009000000000009090909090e09090900000000000011000000000000000000000000000000000700000000000000000000000000000000000000040202020205050505050505050505050505050505050505050505050505050505050505050505000000000000000000000000000000000700000000000000000000000004020202020202010101010101010101010106060606060606060606060606060606060606060606060606060606030000000000000000000800000000000700000004020202020202020201010101010101010101010101010101010106060606060606060606060606060606060606060606060606060606020202020202020202020202020202020202020201010101010101010101010101010101010101010101010101010106060606060606060606060606060606060606060606060606060606010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010106060606060606060606060606060606060606060606060606060606010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010106060606060606060606060606060606060606060606060606060606010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010106060606060606060606060606060606060606060606060606060606`, img`
+................................................2.............222..........
+................................................2............22222.........
+................................................2..........222222222.......
+................................................2.........22222222222......
+................................................2222222222222222222222....2
+................................................2222222.........2.........2
+................................................................2.........2
+................................................................2..........
+........................................2.......................2..........
+................2...................222222222222222222222222222222222222222
+................2............2222222222222222222222222222222222222222222222
+................2...2222222222222222222222222222222222222222222222222222222
+222222222222222222222222222222222222222222222222222222222222222222222222222
+222222222222222222222222222222222222222222222222222222222222222222222222222
+222222222222222222222222222222222222222222222222222222222222222222222222222
+222222222222222222222222222222222222222222222222222222222222222222222222222
+`, [myTiles.transparency16,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.builtin.forestTiles0,sprites.castle.tilePath1,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile13":
+            case "tile15":return tile15;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
