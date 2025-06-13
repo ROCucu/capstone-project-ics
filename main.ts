770d33056d9630728803f95ff8a217250e32427a
ROCucu - MS <michael.strimbu@gmail.com>
2025-06-12 23:33:28 -0700

    Added Player Class, but unsure if gonna be used or not.

--- main.ts
+++ main.ts
@@ -1134,24 +1134,21 @@ Boss_Created = 0
 Jump = 0
 x = 0
 info.changeScoreBy(5)
-mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
+class Player extends sprites.ExtendableSprite{
+    player_health = 200
+    
+    hurt(health: number): void{
+        this.player_health -= health
+        if (this.player_health <= 0){
+            game.gameOver(false)
+        }
+    }
+    constructor(image:Image, SpriteKind: number){
+        super(image,SpriteKind)
+        this.player_health = 200
+    }
+}
+mySprite = sprites.create(assets.image`you`, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
 scene.setBackgroundColor(9)
 tiles.setCurrentTilemap(tilemap`level1`)
